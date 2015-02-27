c3bb706109e8dd581bca4242c5be62be4607fe53
joechina <qingj@hotmail.com>
2015-02-26 21:57:09 -0500

    fix user circular reference issue

--- DataService/Parrot.Model/UserAnnouncement.cs
+++ DataService/Parrot.Model/UserAnnouncement.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Parrot.Model
 {
+    [Serializable]
+    [DataContract(IsReference = true)]
     public class UserAnnouncement
     {
         [Key]
