8f6a72060078137798d125eeb8421b2b87c5d772
Wu-Fei <wufei1@yahoo.com>
2015-09-10 14:05:28 +0800

    quiz widgets update for Admin tool

--- Clients/student/App/viewmodels/myclazz.js
+++ Clients/student/App/viewmodels/myclazz.js
@@ -29,34 +29,45 @@
                 }
             };
         }
+
         function activate() {
             var uid = data.user().Id();
+            /*
+            var now = new Date();
             data.getClasses().then(function (clazz_result) {
                 clazzes.removeAll();
                 clazzes_ids = [];
+
                 clazz_result.results.forEach(function (cur_clazz) {
-                    cur_clazz.teacher = ko.observable();
-                    cur_clazz.state = ko.observable("未加入");
-                    var cur_index = clazzes().length;
-                    clazzes_ids.push(cur_clazz.Id());
-                    clazzes.push(cur_clazz);
-                    if (typeof cur_clazz.TeacherId != "undefined")
-                    {
-                        var callback_fun = makeCallBack(cur_index);
-                        var tid = cur_clazz.TeacherId();
-                        data.getuser(tid).then(callback_fun);
+                    var dif = (now.getTime() - cur_clazz.End()) / 1000 / 60 / 60 / 24;
+                    if (dif < 1) {
+                        cur_clazz.teacher = ko.observable();
+                        cur_clazz.state = ko.observable("未加入");
+                        var cur_index = clazzes().length;
+                        clazzes_ids.push(cur_clazz.Id());
+                        clazzes.push(cur_clazz);
+                        if (typeof cur_clazz.TeacherId != "undefined")
+                        {
+                            var callback_fun = makeCallBack(cur_index);
+                            var tid = cur_clazz.TeacherId();
+                            data.getuser(tid).then(callback_fun);
+                        }
                     }
                 });
+                });
+                */
 
                 data.getUserClasses(uid).then(function (result) {
                     userclazzes(result.results);
+                    /*
                     result.results.forEach(function (my_clazz) {
                         var index = clazzes_ids.indexOf(my_clazz.ClassId());
                         if(index >= 0)
                             clazzes()[index].state("已加入");
                     });
+                    */
                 });
-            });
+            
             
             $("#goback").css({ display: "block" });
             $("#refresh").css({ display: "none" });
