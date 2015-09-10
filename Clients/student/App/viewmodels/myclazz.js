0c384fe1da7c7da04e60fd9b8ee685e243a6d9dd
wu-shiyi <wushiyi@yahoo.com>
2015-09-10 22:16:48 +0100

    Only show joined class in myclass

--- Clients/student/App/viewmodels/myclazz.js
+++ Clients/student/App/viewmodels/myclazz.js
@@ -22,53 +22,41 @@
         return vm;
 
         //#region Internal Methods
-        function makeCallBack(index){
+        function makeCallBack(index) {
             return function (teacher) {
                 if (teacher.results.length > 0) {
                     clazzes()[index].teacher(teacher.results[0].Name());
                 }
             };
         }
-
         function activate() {
             var uid = data.user().Id();
-            /*
-            var now = new Date();
-            data.getClasses().then(function (clazz_result) {
+            data.getUserClasses(uid).then(function (result) {
                 clazzes.removeAll();
                 clazzes_ids = [];
-
-                clazz_result.results.forEach(function (cur_clazz) {
-                    var dif = (now.getTime() - cur_clazz.End()) / 1000 / 60 / 60 / 24;
-                    if (dif < 1) {
-                        cur_clazz.teacher = ko.observable();
-                        cur_clazz.state = ko.observable("未加入");
-                        var cur_index = clazzes().length;
-                        clazzes_ids.push(cur_clazz.Id());
-                        clazzes.push(cur_clazz);
-                        if (typeof cur_clazz.TeacherId != "undefined")
-                        {
-                            var callback_fun = makeCallBack(cur_index);
-                            var tid = cur_clazz.TeacherId();
-                            data.getuser(tid).then(callback_fun);
-                        }
-                    }
+                userclazzes(result.results);
+                result.results.forEach(function (my_clazz) {
+                    clazzes_ids.push(my_clazz.ClassId());
                 });
-                });
-                */
-
-                data.getUserClasses(uid).then(function (result) {
-                    userclazzes(result.results);
-                    /*
-                    result.results.forEach(function (my_clazz) {
-                        var index = clazzes_ids.indexOf(my_clazz.ClassId());
-                        if(index >= 0)
-                            clazzes()[index].state("已加入");
+                if (clazzes_ids.length > 0) {
+                    data.getClasses().then(function (clazz_result) {
+                        clazz_result.results.forEach(function (cur_clazz) {
+                            if (clazzes_ids.indexOf(cur_clazz.Id()) >= 0) {
+                                cur_clazz.teacher = ko.observable();
+                                cur_clazz.state = ko.observable("已加入");
+                                var cur_index = clazzes().length;
+                                clazzes.push(cur_clazz);
+                                if (typeof cur_clazz.TeacherId != "undefined") {
+                                    var callback_fun = makeCallBack(cur_index);
+                                    var tid = cur_clazz.TeacherId();
+                                    data.getuser(tid).then(callback_fun);
+                                }
+                            }
+                        });
                     });
-                    */
-                });
-            
-            
+                }
+            });
+
             $("#goback").css({ display: "block" });
             $("#refresh").css({ display: "none" });
 
