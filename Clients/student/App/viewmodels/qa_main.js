97c86dad0df49c3524cb36fea1a0a04887485791
Wu-Fei <wufei1@yahoo.com>
2015-11-25 22:35:47 +0800

    add qa_main UI files

---
+++ Clients/student/App/viewmodels/qa_main.js
@@ -0,0 +1,34 @@
+﻿define(['plugins/router', 'knockout', 'data', 'logger', 'global'],
+    function (router, ko, data, logger, global) {
+        var qa_cat = ko.observableArray();
+
+        var vm = {
+            activate: activate,
+            router: router,
+
+            qa_cat: global.qa_cat,
+        };
+
+        shouter.subscribe(function (newValue) {
+            activate();
+            logger.log('reload qa categories');
+        }, this, "refresh_viewmodels/qa_main");
+
+        return vm;
+
+        //#region Internal Methods
+        function activate() {
+
+            $("#goback").css({ display: "none" });
+            $("#refresh").css({ display: "none" });
+
+            logger.log('qa_main activated');
+        }
+
+        function goqa() {
+            router.navigate("/#exersize");
+        }
+
+        //#endregion
+
+    });
